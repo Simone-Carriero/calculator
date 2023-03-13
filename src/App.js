5fdba5e74420a8c7addcac3a14fc78ad1ffa20c0
Simone Carriero <simonecarriero95@hotmail.com>
2023-03-13 13:01:08 +0100

    Update buttons and handleClick function
    Create separate array for the calculator buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 
 import { evaluate } from 'mathjs';
+import { calculatorButtons } from './constants/calculatorButtons';
 
-function App() {
+const App = () => {
   const [result, setResult] = useState('');
-
-  const handleClick = ({ target }) => {
-    setResult(result.concat(target.name));
-  };
 
   const clear = () => {
     setResult('');
@@ -27,6 +24,24 @@ function App() {
     }
   };
 
+  const handleClick = ({ target }) => {
+    const { name } = target;
+    switch (name) {
+      case 'AC':
+        clear();
+        break;
+      case 'DEL':
+        deleteScreen();
+        break;
+      case '=':
+        calculate();
+        break;
+      default:
+        setResult(result.concat(name));
+        break;
+    }
+  };
+
   return (
     <main>
       <section className='calculator'>
@@ -40,120 +55,23 @@ function App() {
         </form>
 
         <div className='calculator__body'>
-          <button
-            onClick={clear}
-            className='calculator__button span ac'>
-            AC
-          </button>
-          <button
-            onClick={deleteScreen}
-            className='calculator__button'>
-            DEL
-          </button>
-          <button
-            name='/'
-            onClick={handleClick}
-            className='calculator__button'>
-            &divide;
-          </button>
+          {calculatorButtons.map((button, i) => {
+            const { name, className, symbol } = button;
 
-          <button
-            name='1'
-            onClick={handleClick}
-            className='calculator__button'>
-            1
-          </button>
-          <button
-            name='2'
-            onClick={handleClick}
-            className='calculator__button'>
-            2
-          </button>
-          <button
-            name='3'
-            onClick={handleClick}
-            className='calculator__button'>
-            3
-          </button>
-          <button
-            name='*'
-            onClick={handleClick}
-            className='calculator__button'>
-            &lowast;
-          </button>
-
-          <button
-            name='4'
-            onClick={handleClick}
-            className='calculator__button'>
-            4
-          </button>
-          <button
-            name='5'
-            onClick={handleClick}
-            className='calculator__button'>
-            5
-          </button>
-          <button
-            name='6'
-            onClick={handleClick}
-            className='calculator__button'>
-            6
-          </button>
-          <button
-            name='+'
-            onClick={handleClick}
-            className='calculator__button'>
-            +
-          </button>
-
-          <button
-            name='7'
-            onClick={handleClick}
-            className='calculator__button'>
-            7
-          </button>
-          <button
-            name='8'
-            onClick={handleClick}
-            className='calculator__button'>
-            8
-          </button>
-          <button
-            name='9'
-            onClick={handleClick}
-            className='calculator__button'>
-            9
-          </button>
-          <button
-            name='-'
-            onClick={handleClick}
-            className='calculator__button'>
-            &minus;
-          </button>
-
-          <button
-            name='.'
-            onClick={handleClick}
-            className='calculator__button'>
-            .
-          </button>
-          <button
-            name='0'
-            onClick={handleClick}
-            className='calculator__button'>
-            0
-          </button>
-          <button
-            onClick={calculate}
-            className='calculator__button span'>
-            {' '}
-            ={' '}
-          </button>
+            return (
+              <button
+                key={i}
+                onClick={handleClick}
+                className={className}
+                name={name}>
+                {name || symbol}
+              </button>
+            );
+          })}
         </div>
       </section>
     </main>
   );
-}
+};
 
 export default App;
